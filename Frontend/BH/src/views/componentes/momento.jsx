dc18c4c8a413c02af5212edf05f39b73e4c6e0fb
bautista <brazibautista@gmail.com>
2025-09-10 22:57:41 -0300

    animaciones momentos #9

--- Frontend/BH/src/views/componentes/momento.jsx
+++ Frontend/BH/src/views/componentes/momento.jsx
@@ -9,25 +9,41 @@ export default function Momento ({texto, foto, lado_foto, visible, index}) {
 
     const scrollY = useSelector((state) => state.scrolls.scrollY ?? 0);
     const distance = useSelector((state) => state.scrolls.distance ?? window.innerHeight);
-
-    const {  opacidad } = useMemo(() => {
-        const progress = Math.min(scrollY / ((distance * 0.75) * (index + 1 )), 1);
     
+    const {opacidad, desp} = useMemo(() => {
+        const progress = Math.min(scrollY / (((distance * 0.75) * (index + 1 )) * 0.85), 1);
+        const offsetScroll = scrollY - ((distance * 0.75) * (index + 1 )) - distance * -0.25;
+        const progressDesp = offsetScroll > 0 ? Math.min(offsetScroll / (distance * 0.25), 1) : 0;
+        const desplazamiento = 110 * (1 - progressDesp)
+
         return {
-          opacidad: progress < 1 ? 0 : 1,
+            opacidad: progress < 1 ? 0.3 : 1,
+            desp: lado_foto == "right" ? desplazamiento : desplazamiento * -1,
         };
     }, [scrollY, distance, index]);
-    
-    const divMomentoStyle = useMemo(() => ({ opacity: opacidad,
-        transition: '0.2s',
+
+    const divMomentoStyle = useMemo(() => ({ 
+        opacity: opacidad,
+        transition: '0.3s',
     }), [opacidad]);
 
     const historiaFotoStyle = useMemo(() => ({         
         width: `40vw`,
         height: `30vw`,
         objectFit: `cover`,
-        transform: `translateX(${scrollY * 0.1}px)`
-    }), [scrollY]);
+        transform: `translateX(${desp}vh)`
+    }), [scrollY, desp]);
+
+
+
+
+
+
+
+
+
+
+
 
     if (!visible) {
         return null;
@@ -38,15 +54,15 @@ export default function Momento ({texto, foto, lado_foto, visible, index}) {
                 {lado_foto === "left" ? 
                 <>
                     <div className='grid'>
-                        <img src={imgPath} className={`historia-foto ${lado_foto}`} style={historiaFotoStyle}/>
+                        <img src={imgPath} className={`historia-foto center`} style={historiaFotoStyle}/>
                     </div>
-                    <div className='grid center historia-texto-right'><p>{texto}</p></div>
+                    <div className='grid historia-texto-right'><p>{texto}</p></div>
                 </>
                 :
                 <>
-                    <div className='grid center historia-texto-left'><p>{texto}</p></div>
+                    <div className='grid historia-texto-left'><p>{texto}</p></div>
                     <div className='grid'>
-                        <img src={imgPath} className={`historia-foto ${lado_foto}`} style={historiaFotoStyle}/>
+                        <img src={imgPath} className={`historia-foto center`} style={historiaFotoStyle}/>
                     </div>
                 </>
                 }
